64e8842f6ea3edb96e6a18a1c2a9a9a59c138009
SoahamMoulree <soaham.moulree@gmail.com>
2025-08-14 21:20:34 +0530

    Create alu_pkg.sv

---
+++ CODE/alu_pkg.sv
@@ -0,0 +1,14 @@
+`include "uvm_macros.svh"
+package alu_pkg;
+  import uvm_pkg::*;
+  `include "seq_item.sv"
+  `include "sequence.sv"
+  `include "sequencer.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "agent.sv"
+  `include "scoreboard.sv"
+  `include "coverage.sv"
+  `include "environment.sv"
+  `include "test.sv"
+endpackage
