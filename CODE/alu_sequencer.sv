7185c093b7a34decbf2763687249363e5ea81c2f
SoahamMoulree <soaham.moulree@gmail.com>
2025-08-14 21:13:45 +0530

    Create alu_sequencer.sv

---
+++ CODE/alu_sequencer.sv
@@ -0,0 +1,9 @@
+class alu_seqr extends uvm_sequencer#(seq_item);
+
+  `uvm_component_utils(alu_seqr)
+
+  function new(string name,uvm_component parent);
+    super.new(name,parent);
+  endfunction
+
+  endclass
